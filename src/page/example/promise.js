cf904cf0a97843647d27cca466b53640342fe949
师小强 <huiqiang.shi@wenba100.com>
2018-03-05 15:39:32 +0800

    新增注释

--- src/page/example/promise.js
+++ src/page/example/promise.js
@@ -4,17 +4,20 @@ class MyPromise {
 		this.status = 'pending';
 		this.value = void 0;
 		this.deferreds = [];
+		// 触发第一次的回调
 		fn(this.resolve, this.reject);
 	}
 	handle = deferred => {
-		// 这是一个递归、当正在执行的时候，
-		// 运行到then的时候会塞入队列中，
-		// 执行完后，从队列中取一个继续执行
 		if (this.status === 'pending') {
+			// 这是一个递归、当正在执行的时候，
+			// 运行到then的时候会塞入队列中，
+			// 执行完后，从队列中取一个继续执行
 			this.deferreds.push(deferred);
 			return;
 		}
+		// 若状态已经切换到fulfilled，设置回调函数
 		let callback = this.status === 'fulfilled' ? deferred.onFulfilled : deferred.onRejected;
+		// 若无回调
 		if (callback === null) {
 			callback = this.status === 'fulfilled' ? deferred.resolve : deferred.reject;
 			callback(this.value);
@@ -22,12 +25,15 @@ class MyPromise {
 		}
 		try {
 			const result = callback(this.value);
+			// 执行回调函数，更新状态并重新赋给新值
 			deferred.resolve(result);
 		} catch (e) {
+			// 报错踢出，更新状态
 			deferred.reject(e);
 		}
 	};
 	then = (onFulfilled, onRejected) => {
+		//确保每次都返回promise
 		return new MyPromise((resolve, reject) => {
 			this.handle({
 				onFulfilled: onFulfilled || null,
@@ -57,7 +63,6 @@ class MyPromise {
 	};
 
 	finale = () => {
-		console.log('finale');
 		setTimeout(() => {
 			console.log(this.deferreds);
 			this.deferreds.forEach(deferred => {
