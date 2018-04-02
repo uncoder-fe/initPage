3d6d30ef3ee637432f362c337083a8c3387721a3
师小强 <huiqiang.shi@wenba100.com>
2018-04-02 16:41:08 +0800

    更新

--- src/components/handwriting/control-panel/index.jsx
+++ src/components/handwriting/control-panel/index.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import DragInput from './drag-input';
-import playIcon from './img/play.png';
-import pauseIcon from './img/pause.png';
-import replayIcon from './img/replay.png';
+// import playIcon from './img/play.png';
+// import pauseIcon from './img/pause.png';
+// import replayIcon from './img/replay.png';
 
 import './index.less';
+
+const playIcon = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAARVBMVEUAAABBe/9AgP9Bev9Cev9Bev9Aev9Aef9Cev9Bev9Be/9Ce/9AeP9Cev9Be/9Cev9Cev9Ce/9Be/9Ae/9AfP9Bef9Ce/9ju6GTAAAAFnRSTlMA0BDwgMAwUJDfcOAgf6CfYI+wX0A/JCxAUgAAAPxJREFUOMuFU1myxCAIFMEoGk3yZh73P+ps5ULGmvCRlNg0i40ZzG8ORMAd3kzMbihAtO9EILjZ8z1hims9rDFh1uGAZJWDEAbHwrCcKRfg5rNc0ZqVqxf6vUZAra/z6yxIb+DrNzfCF3VJQ8y/TpLC84uxOe4iup2IxnhZ2znI08YJreJNgX52IqJJIBhHGqBJgjMwAcjW+gAj+wQgNUuUS8A8hesppkX+2aHI8NUm+rFNNajSwvug1Kh9D++jNoV7yN1r4XG4fO4rwfyWHEPTxVy0yTauNJE9p0UtTtb1ZzyxEvK4evzdmi0ocOTbLR8gGKZl+/BZ/zBO8wHUqw0rxV0YPQAAAABJRU5ErkJggg==';
+const pauseIcon = 'data: image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAANlBMVEUAAABBev9AeP9AfP9AgP9Cev9Be/9Be / 9Bev9Bev9Be / 9Bev9Be / 9Cev9Aef9Aev9Cev9Ce / 9tblxbAAAAEXRSTlMA8CBAEIDQoM / AcGDgf1AwkFoE2fEAAADZSURBVDjLhZPZEsMgCEXBhsUtKf//s51m2mBWeHBGOYAKFwabJauZZpnhwpKgMVGtRGwo6eCeOraaNrg27NPOr1j2EQV1IBbkdCrJuGzxmJ32rBmnf352/0jwrwrhIb9XprUYliuvh0rzg7RG9O2gCQBg3fb2+q4vGx4LMFu6B5LNIAz3ALBApieAMvAzoGD1CSgWAlEJDi75ziAaPDP6qOirL5tFQ7PidscD46Pl5qMYDa3n0nTKr7gMNGM/3o/30iJsxaVXGtJ0IV7tvdbe9SRelz+b8V7+H//NCFG8swogAAAAAElFTkSuQmCC';
+const replayIcon = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAOVBMVEUAAABAe/9Cev9AgP9Bev9Bev9Bev9AeP9Be/9Bev9Be/9Ce/9Bev9Be/9Aev9Aef9Cev9Aev9Ce/9T7oVaAAAAEnRSTlMAQIAQYPDgINDPoHDAsDBQkG/HXjLgAAAA00lEQVQ4y62TSZbDIAxENTAYx0O67n/YxiFEPHDsTf7Cz0AhVELQb1H+ExFWOseteLM4GuGEhsBEtK9NrMcxK9vrHBfyQIh8/lRmwLsm3ATMdKhs/xSpQbNCTMB5XTtDC2CCCT5SR/QmcMCJLzHBitBv54yvAq0/hkdBaopbJwgo7NXjWNcX8ZMNXXEveNwJLMkRs3kCLxtZoUY0YKabUjNdXJZ6LG2aqb/uVAKY0xT79ScZQ8sllAy7pn0evmJp2nl0jQYvlw8nOP329JzIzko/5R9iTgy3DxvgMgAAAABJRU5ErkJggg==';
 
 class ControlPanel extends Component {
 	constructor() {
