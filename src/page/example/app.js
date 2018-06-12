be823eefb88d4682a21d26b92c8fb46ed1045a16
师小强 <huiqiang.shi@wenba100.com>
2018-06-12 15:15:58 +0800

    更新

--- src/page/example/app.js
+++ src/page/example/app.js
@@ -1,5 +1,4 @@
 import { HashRouter, Route, Switch, Link } from 'react-router-dom';
-
 import IndexPage from './index';
 import detailPage from './detail';
 
@@ -23,6 +22,27 @@ const App = () => (
 // 渲染
 ReactDOM.render(<App />, document.querySelector('#app'));
 
-const App2 = () => <div>我是另一个app</div>;
+const App2 = class extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			list: []
+		};
+	}
+	componentDidMount() {
+		setInterval(() => {
+			const { list } = this.state;
+			list.push(Date.now());
+			this.setState({ list });
+		}, 5000);
+	}
+	render() {
+		const { list } = this.state;
+		const listNodes = list.map((item, index) => {
+			return <h1 key={index}>{item}</h1>;
+		});
+		return <div>我是第二个app{listNodes}</div>;
+	}
+};
 
 ReactDOM.render(<App2 />, document.querySelector('#app2'));
