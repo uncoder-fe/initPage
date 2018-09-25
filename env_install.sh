ae7c78ed1d38f6bce53b943c6cd4a9045b7372e3
uncoder-fe <838545898@qq.com>
2018-09-25 19:34:43 +0800

    更新安足昂

--- env_install.sh
+++ env_install.sh
@@ -1,12 +1,27 @@
 #! /bin/bash
 # environment centos 7
-# 快速安装 nodejs, yarn, pm2
+# 快速安装 nginx, nodejs, yarn, pm2
+
+# 修改脚本可执行权限 chmod u+x test.sh
+# 查看安装路径 rpm -ql nginx
+# $basearch是我们的系统硬件架构(CPU指令集), 使用命令arch得到
+
+# 全局变量
+basearch=$(arch)
+NGINX_VERSION=http://nginx.org/packages/centos/7/${basearch}/
+NODEJS_VERSION=https://rpm.nodesource.com/setup_10.x
+YARN_VERSION=https://dl.yarnpkg.com/rpm/yarn.repo
+
+# 检测nginx是否安装
+if type nginx >/dev/null 2>&1; then
+    echo 'exists nginx'
+else
+    echo 'no exists nginx'
+    echo -e "[nginx]\n\name=nginx repo\nbaseurl=${NGINX_VERSION}\ngpgcheck=0\nenabled=1" > /etc/yum.repos.d/nginx.repo
+    yum install nginx
+fi
 
 # 检测nodejs是否安装
-
-NODEJS_VERSION = https://rpm.nodesource.com/setup_10.x
-YARN_VERSION = https://dl.yarnpkg.com/rpm/yarn.repo
-
 if type node >/dev/null 2>&1; then
     echo 'exists nodejs'
 else
