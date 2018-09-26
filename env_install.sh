12a6a8a853b38cfa697ba9543795b38f26b0d144
uncoder-fe <838545898@qq.com>
2018-09-26 11:18:13 +0800

    追加

--- env_install.sh
+++ env_install.sh
@@ -16,40 +16,61 @@ YARN_VERSION=https://dl.yarnpkg.com/rpm/yarn.repo
 
 # 检测nginx是否安装
 if type nginx >/dev/null 2>&1; then
-    echo 'exists nginx'
+    echo 'nginx已经存在...'
 else
-    echo 'no exists nginx'
+    echo 'nginx不存在，开始下载.....'
     echo -e "[nginx]\n\name=nginx repo\nbaseurl=${NGINX_VERSION}\ngpgcheck=0\nenabled=1" > /etc/yum.repos.d/nginx.repo
     yum -y install nginx
+    echo 'nginx安装完毕.....'
+fi
+
+# 检测ss是否安装
+if type ss-server >/dev/null 2>&1; then
+    echo 'ss已经存在...'
+else
+    echo 'ss不存在，开始下载.....'
+    curl -o/etc/yum.repos.d/shadowsocks-libev.repo https://copr.fedorainfracloud.org/coprs/librehat/shadowsocks/repo/epel-7/librehat-shadowsocks-epel-7.repo
+    yum -y install shadowsocks-libev
+    echo '配置文件路径：/etc/shadowsocks-libev/config.json'
+    echo '服务路径：/usr/lib/systemd/system/shadowsocks-libev.service'
+    echo '追加：ExecStart=/usr/bin/ss-server -c /etc/shadowsocks-libev/config.json'
+    echo '追加：ExecReload=/bin/kill -HUP $MAINPID'
+    echo '追加：ExecStop=/bin/kill -s QUIT $MAINPID'
+    echo '追加：PrivateTmp=true'
+    echo '追加：KillMode=process'
+    echo 'ss安装完毕，请复制上面的追加内容，手工调整服务.....'
 fi
 
 # 检测nodejs是否安装
 if type node >/dev/null 2>&1; then
-    echo 'exists nodejs'
+    echo 'nodejs已经存在...'
 else
-    echo 'no exists nodejs'
+    echo 'nodejs不存在，开始下载.....'
     curl --silent --location ${NODEJS_VERSION} | sudo bash -
     yum -y install nodejs
+    echo 'nodejs安装完毕.....'
 fi
 
 # 检测yarn是否安装
 if type yarn >/dev/null 2>&1; then
-    echo 'exists yarn'
+    echo 'yarn已经存在...'
 else
-    echo 'no exists yarn'
+    echo 'yarn不存在，开始下载.....'
     curl --silent --location ${YARN_VERSION} | sudo tee /etc/yum.repos.d/yarn.repo
     yum -y install yarn
+    echo 'yarn安装完毕.....'
 fi
 
 # 检测pm2是否安装
 if type pm2 >/dev/null 2>&1; then
-    echo 'exists pm2'
+    echo 'pm2已经存在...'
 else
-    echo 'no exists pm2'
+    echo 'pm2不存在，开始下载.....'
     yarn add pm2 -g
+    echo 'pm2安装完毕.....'
 fi
 
-echo '安装结束了....'
+echo '安装已经全部结束....'
 
 }
 
