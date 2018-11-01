05d870697e9fdb4358a44bbe150c42b2747f40e5
uncoder-fe <838545898@qq.com>
2018-11-01 20:47:07 +0800

    跟新

---
+++ src/components/editor/config.js
@@ -0,0 +1,14 @@
+const domain = 'http://phhwytweo.bkt.clouddn.com/';
+const uptoken =
+	'TX-sku9Zgpw1oXXErzq1mRCIM1f_x3a-rtBkyhSN:PZehcEr-rJCWlHArVo2Po70E-pY=:eyJzY29wZSI6ImltYWdlIiwiZGVhZGxpbmUiOjE1NDExMDMyOTh9';
+
+const config = {
+	useCdnDomain: false,
+	region: null,
+	uphost: 'up-z0.qiniup.com'
+};
+const putExtra = {
+	params: {},
+	mimeType: ['image/png', 'image/jpeg', 'image/jpg']
+};
+export default { domain, uptoken, config, putExtra };
--- src/components/editor/index.jsx
+++ src/components/editor/index.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import {
-	CompositeDecorator,
 	AtomicBlockUtils,
+	SelectionState,
+	Modifier,
 	Editor,
 	EditorState,
-	ContentState,
 	convertFromRaw,
 	convertToRaw
 } from 'draft-js';
-
+import qnConfig from './config';
 import MyImg from './my-img';
-
-const styles = {
-	editor: {
-		border: '1px solid gray',
-		minHeight: '6em'
-	}
-};
-
+import './index.less';
+debugger;
 export default class MyEditor extends React.Component {
 	constructor(props) {
 		super(props);
@@ -25,55 +19,97 @@ export default class MyEditor extends React.Component {
 			editorState: EditorState.createEmpty()
 		};
 	}
-	componentWillMount() {
-		const init = {
-			blocks: [
-				{
-					key: '4d5od',
-					text: '',
-					type: 'unstyled',
-					depth: 0,
-					inlineStyleRanges: [],
-					entityRanges: [],
-					data: {}
-				},
-				{
-					key: 'an8bv',
-					text: ' ',
-					type: 'atomic',
-					depth: 0,
-					inlineStyleRanges: [],
-					entityRanges: [{ offset: 0, length: 1, key: 0 }],
-					data: {}
-				},
-				{
-					key: '2u2of',
-					text: '',
-					type: 'unstyled',
-					depth: 0,
-					inlineStyleRanges: [],
-					entityRanges: [],
-					data: {}
-				}
-			],
-			entityMap: {
-				'0': {
-					type: 'image',
-					mutability: 'IMMUTABLE',
-					data: { text: '![图片]()', src: 'https://www.baidu.com/img/bd_logo1.png' }
-				}
+	onChange = editorState => {
+		this.setState({ editorState }, () => {
+			console.log('convertFromRaw', JSON.stringify(convertToRaw(editorState.getCurrentContent())));
+		});
+	};
+	qiniuUp = (imgSource, cb) => {
+		const file = imgSource;
+		const key = null;
+		const observable = window.qiniu.upload(file, key, qnConfig.uptoken, qnConfig.putExtra, qnConfig.config);
+		observable.subscribe({
+			next(res) {
+				// console.log(res);
+			},
+			error(err) {
+				console.log(err);
+			},
+			complete: res => {
+				const { key } = res;
+				const imageUrl = domain + key;
+				console.log('imageurl', imageUrl);
+				this.insertImage(imageUrl);
 			}
-		};
-		const blockArry = convertFromRaw(init);
-		const editorState = EditorState.createWithContent(blockArry);
-		this.setState({ editorState });
+		});
+	};
+	uploadImg = e => {
+		const file = e.target.files[0];
+		const fileMaxSize = 500000;
+		if (!/image\/\w+/.test(file.type)) {
+			console.log('选择的文件不是图片');
+		}
+		if (file.size >= fileMaxSize) {
+			console.log('文件过大');
+		}
+		this.qiniuUp(file);
+	};
+	componentWillMount() {
+		if (false) {
+			const init = {
+				blocks: [
+					{
+						key: '4d5od',
+						text: '',
+						type: 'unstyled',
+						depth: 0,
+						inlineStyleRanges: [],
+						entityRanges: [],
+						data: {}
+					},
+					{
+						key: 'an8bv',
+						text: ' ',
+						type: 'atomic',
+						depth: 0,
+						inlineStyleRanges: [],
+						entityRanges: [{ offset: 0, length: 1, key: 0 }],
+						data: {}
+					},
+					{
+						key: '2u2of',
+						text: '',
+						type: 'unstyled',
+						depth: 0,
+						inlineStyleRanges: [],
+						entityRanges: [],
+						data: {}
+					}
+				],
+				entityMap: {
+					0: {
+						type: 'image',
+						mutability: 'IMMUTABLE',
+						data: {
+							text: '![图片]()',
+							src: 'http://phhwytweo.bkt.clouddn.com/3ef0a61fe5234429a03749a29851f6c0.jpeg'
+						}
+					}
+				}
+			};
+			const blockArry = convertFromRaw(init);
+			const editorState = EditorState.createWithContent(blockArry);
+			this.setState({ editorState });
+		} else {
+			this.setState({ editorState: EditorState.createEmpty() });
+		}
 	}
-	insertImage = img => {
+	insertImage = imageUrl => {
 		const { editorState } = this.state;
 		const contentState = editorState.getCurrentContent();
 		const contentStateWithEntity = contentState.createEntity('image', 'IMMUTABLE', {
-			text: '裁剪图片',
-			src: 'https://www.baidu.com/img/bd_logo1.png'
+			text: '我是题目图片',
+			src: imageUrl
 		});
 		const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
 		const newEditorState = EditorState.set(editorState, { currentContent: contentStateWithEntity });
@@ -82,13 +118,53 @@ export default class MyEditor extends React.Component {
 				editorState: AtomicBlockUtils.insertAtomicBlock(newEditorState, entityKey, ' ')
 			},
 			() => {
-				setTimeout(() => this.myEditor.focus(), 0);
+				// setTimeout(() => this.myEditor.focus(), 0);
 			}
 		);
 	};
-	onChange = editorState => {
-		this.setState({ editorState }, () => {
-			console.log('convertFromRaw', JSON.stringify(convertToRaw(editorState.getCurrentContent())));
+	delImg = block => {
+		const blockKey = block.getKey();
+		const blockLength = block.getLength();
+		const { editorState } = this.state;
+		const contentState = editorState.getCurrentContent();
+		// You need to create a selection for entire length of text in the block
+		const selection = SelectionState.createEmpty(blockKey);
+		const updatedSelection = selection.merge({
+			anchorKey: blockKey,
+			// anchorOffset is the start of the block
+			anchorOffset: 0,
+			focusKey: blockKey,
+			// focustOffset is the end
+			focusOffset: blockLength
+		});
+		// 删除entry
+		const newContentState = Modifier.applyEntity(contentState, updatedSelection, null);
+		// const newEditorState = EditorState.push(editorState, newContentState, 'apply-entity');
+		const newEditorState = EditorState.set(editorState, { currentContent: newContentState });
+		// 删除block
+		const contentStateWithoutBlock = Modifier.removeRange(newContentState, updatedSelection, 'backward');
+		const newEditorState2 = EditorState.push(newEditorState, contentStateWithoutBlock, 'remove-range');
+		// 以上操作想法是对的，结果不太尽人意
+		this.setState(
+			{
+				editorState: newEditorState2
+			},
+			() => {
+				console.log(
+					'delete/convertFromRaw',
+					JSON.stringify(convertToRaw(newEditorState2.getCurrentContent()).blocks.length)
+				);
+			}
+		);
+	};
+	cropImg = (block, imgFile) => {
+		// console.log(block, imgFile);
+		imgFile.name = `image-${Date.now()}.jpeg`;
+		this.qiniuUp(imgFile, src => {
+			debugger;
+			const { editorState } = this.state;
+			const contentState = editorState.getCurrentContent();
+			const entry = contentState.replaceEntityData(block.getKey(), { src });
 		});
 	};
 	myBlockRenderer = contentBlock => {
@@ -97,7 +173,11 @@ export default class MyEditor extends React.Component {
 			return {
 				// 指定组件
 				component: MyImg,
-				editable: false
+				editable: false,
+				props: {
+					del: this.delImg,
+					crop: this.cropImg
+				}
 			};
 		}
 		return null;
@@ -105,9 +185,19 @@ export default class MyEditor extends React.Component {
 	render() {
 		const { editorState } = this.state;
 		return (
-			<div style={styles.editor}>
-				<div>
-					<div onClick={this.insertImage}>图片</div>
+			<div className="my-editor">
+				<div className="editor-control">
+					<div>
+						<label htmlFor="avatar">
+							选择图片:
+							<input
+								id="avatar"
+								type="file"
+								accept="image/png, image/jpeg, image/jpg"
+								onChange={this.uploadImg}
+							/>
+						</label>
+					</div>
 				</div>
 				<Editor
 					ref={editor => (this.myEditor = editor)}
